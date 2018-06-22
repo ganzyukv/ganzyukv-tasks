907ebe4c221f3635db214eb14404e8d0ae3a9694
Vladislav Ganzyuk <v.ganzyuk@hexa.com.ua>
2018-06-22 18:27:16 +0300

    Task 1 is done

--- task1/index.php
+++ task1/index.php
@@ -8,7 +8,42 @@
  */
 function fillSpiralMatrix($n)
 {
+    $curSide = $side = $n;
+    $offset = 0;
+    $curNumber = 1;
     $matrix = [];
-    // Ваш код
+
+    while ($offset < ($side / 2)) {
+
+        // top line
+        for ($i = 0; $i < $curSide; $i++) {
+            $matrix[$offset][$offset + $i] = $curNumber++;
+        }
+
+        // right line
+        for ($i = 0; $i < $curSide - 2; $i++) {
+            $matrix[1 + $offset + $i][$side - 1 - $offset] = $curNumber++;
+        }
+
+        // bottom line
+        if (floor($side / 2) - $offset > 0) {
+            for ($i = 0; $i < $curSide; $i++) {
+                $matrix[$side - 1 - $offset][$side - 1 - $i - $offset] = $curNumber++;
+            }
+        }
+
+        // left line
+        for ($i = 0; $i < $curSide - 2; $i++) {
+            $matrix[$side - 2 - $offset - $i][$offset] = $curNumber++;
+        }
+
+        $offset++;
+        $curSide -= 2;
+    }
+
+    foreach ($matrix as &$row) {
+        ksort($row);
+    }
+
     return $matrix;
 }
